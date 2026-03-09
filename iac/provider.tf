e5421ea85018dfecd894978703b1e137fc797445
mhakimov <xakim@inbox.lv>
2026-03-09 17:18:55 +0100

    added ingress.yaml

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "azurerm" {
+  # Change these values
+    resource_group_name  = "terraform-remote-state"
+    storage_account_name = "atlassianterraformrs"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
